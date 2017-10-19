58d789acd202e46567091d46fecdcf31d10e9b13
Adam <dobias.adam96@gmail.com>
2017-10-19 09:34:23 +0200

    tag and ver

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -33,4 +34,19 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
 	}
 
+        @Override
+        public void sellCar(Car car) throws RequestedCarNotFoundException{
+            
+            if ( ! isCarAvailable(car.getColor(), car.getType()).isPresent() ) {
+                throw new RequestedCarNotFoundException("car of type "+car.getType()
+                        +" and color "+car.getColor()+" was not found");
+            } else {
+                CarShopStorage.getInstancce().getCars().get(car.getType())
+                        .remove(car);
+            }
+            
+            
+            
+        }
+        
 }
